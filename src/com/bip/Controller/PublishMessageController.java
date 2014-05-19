f57522c8322b92a59c9c06f5002e4d483a2576e9
Scalzdp <275735763@qq.com>
2014-05-19 11:16:44 +0800

    modify publishmessage

--- src/com/bip/Controller/PublishMessageController.java
+++ src/com/bip/Controller/PublishMessageController.java
@@ -8,10 +8,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import net.sf.json.JSONObject;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -130,7 +133,7 @@ public class PublishMessageController {
 	}
 	
 	@RequestMapping(value="published/saveedit")
-	public String acceptEditDg(Model model){
+	public String acceptEditDg(Model model,HttpServletRequest request){
 		return "";
 	} 
 	
